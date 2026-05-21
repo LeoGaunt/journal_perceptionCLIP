5396b63aaabf86846f88e0f15bdec2f803de3504
Leo Gaunt <36968548+LeoGaunt@users.noreply.github.com>
2026-05-21 23:00:10 +0100

    Added scipy to setup

--- colab_setup.sh
+++ colab_setup.sh
@@ -2,6 +2,7 @@ echo "Installing dependencies"
 pip install open_clip_torch
 pip install git+https://github.com/modestyachts/ImageNetV2_pytorch
 pip install kornia
+pip install scipy
 echo "Installing Grad-CAM dependencies"
 #pip install git+https://github.com/openai/CLIP.git
 #pip install torchray
